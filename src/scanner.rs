9aa964e10ade5cfef5c943d6a76d5e789d4fafba
nimai <nimai.m.patel@gmail.com>
2023-06-16 23:18:58 +0530

    shorter variable names

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,74 +1,85 @@
 use std::error::Error;
 
 use crate::{
+    error,
     token::Token,
     token_type::{string_to_keyword, TokenType},
 };
 
-pub fn scan<'a>(source: &'a str) -> Result<Vec<Token<'a>>, Box<dyn Error>> {
+pub fn scan<'a>(src: &'a str) -> Result<Vec<Token<'a>>, Box<dyn Error>> {
     let mut line = 1;
     let mut tokens = Vec::new();
-    let mut chars = source.char_indices().peekable();
-    while let Some((cur_idx, c)) = chars.next() {
+    let mut chars = src.char_indices().peekable();
+    while let Some((idx, c)) = chars.next() {
         match c {
             '{' => {
-                tokens.push(Token::new(
-                    TokenType::LeftBrace,
-                    &source[cur_idx..=cur_idx],
-                    line,
-                ));
+                tokens.push(Token::new(TokenType::LeftBrace, &src[idx..=idx], line));
             }
             '}' => {
-                tokens.push(Token::new(
-                    TokenType::RightBrace,
-                    &source[cur_idx..=cur_idx],
-                    line,
-                ));
+                tokens.push(Token::new(TokenType::RightBrace, &src[idx..=idx], line));
             }
             '(' => {
-                tokens.push(Token::new(
-                    TokenType::LeftParen,
-                    &source[cur_idx..=cur_idx],
-                    line,
-                ));
+                tokens.push(Token::new(TokenType::LeftParen, &src[idx..=idx], line));
             }
             ')' => {
-                tokens.push(Token::new(
-                    TokenType::RightParen,
-                    &source[cur_idx..=cur_idx],
-                    line,
-                ));
+                tokens.push(Token::new(TokenType::RightParen, &src[idx..=idx], line));
+            }
+            '.' => {
+                tokens.push(Token::new(TokenType::Dot, &src[idx..=idx], line));
+            }
+            '-' => {
+                tokens.push(Token::new(TokenType::Minus, &src[idx..=idx], line));
+            }
+            '+' => {
+                tokens.push(Token::new(TokenType::Plus, &src[idx..=idx], line));
+            }
+            ',' => {
+                tokens.push(Token::new(TokenType::Comma, &src[idx..=idx], line));
+            }
+            ';' => {
+                tokens.push(Token::new(TokenType::Semicolon, &src[idx..=idx], line));
+            }
+            '*' => {
+                tokens.push(Token::new(TokenType::Star, &src[idx..=idx], line));
             }
             '=' => {
                 if let Some((end_idx, '=')) = chars.peek() {
                     tokens.push(Token::new(
                         TokenType::EqualEqual,
-                        &source[cur_idx..=*end_idx],
+                        &src[idx..=*end_idx],
                         line,
                     ));
                     chars.next();
                 } else {
-                    tokens.push(Token::new(
-                        TokenType::Equal,
-                        &source[cur_idx..=cur_idx],
-                        line,
-                    ));
+                    tokens.push(Token::new(TokenType::Equal, &src[idx..=idx], line));
                 }
             }
             '!' => {
                 if let Some((end_idx, '=')) = chars.peek() {
+                    tokens.push(Token::new(TokenType::BangEqual, &src[idx..=*end_idx], line));
+                    chars.next();
+                } else {
+                    tokens.push(Token::new(TokenType::Bang, &src[idx..=idx], line));
+                }
+            }
+            '>' => {
+                if let Some((end_idx, '=')) = chars.peek() {
                     tokens.push(Token::new(
-                        TokenType::BangEqual,
-                        &source[cur_idx..=*end_idx],
+                        TokenType::GreaterEqual,
+                        &src[idx..=*end_idx],
                         line,
                     ));
                     chars.next();
                 } else {
-                    tokens.push(Token::new(
-                        TokenType::Bang,
-                        &source[cur_idx..=cur_idx],
-                        line,
-                    ));
+                    tokens.push(Token::new(TokenType::Greater, &src[idx..=idx], line));
+                }
+            }
+            '<' => {
+                if let Some((end_idx, '=')) = chars.peek() {
+                    tokens.push(Token::new(TokenType::LessEqual, &src[idx..=*end_idx], line));
+                    chars.next();
+                } else {
+                    tokens.push(Token::new(TokenType::Less, &src[idx..=idx], line));
                 }
             }
             '/' => {
@@ -81,18 +92,14 @@ pub fn scan<'a>(source: &'a str) -> Result<Vec<Token<'a>>, Box<dyn Error>> {
                         }
                     }
                 } else {
-                    tokens.push(Token::new(
-                        TokenType::Slash,
-                        &source[cur_idx..=cur_idx],
-                        line,
-                    ));
+                    tokens.push(Token::new(TokenType::Slash, &src[idx..=idx], line));
                 }
             }
             '"' => {
                 while let Some((end_idx, maybe_end_quote)) = chars.next() {
                     match maybe_end_quote {
                         '"' => {
-                            let lexeme = &source[cur_idx..=end_idx];
+                            let lexeme = &src[idx..=end_idx];
                             tokens.push(Token::new(
                                 TokenType::String(&lexeme[1..lexeme.len() - 1]),
                                 lexeme,
@@ -106,7 +113,7 @@ pub fn scan<'a>(source: &'a str) -> Result<Vec<Token<'a>>, Box<dyn Error>> {
                 }
             }
             some_digit if some_digit.is_digit(10) => {
-                let mut end_idx = cur_idx;
+                let mut end_idx = idx;
                 while let Some((_, digit)) = chars.peek() {
                     if !digit.is_digit(10) {
                         break;
@@ -114,7 +121,7 @@ pub fn scan<'a>(source: &'a str) -> Result<Vec<Token<'a>>, Box<dyn Error>> {
                     end_idx += 1;
                     chars.next();
                 }
-                let lexeme = &source[cur_idx..=end_idx];
+                let lexeme = &src[idx..=end_idx];
                 tokens.push(Token::new(
                     TokenType::Number(lexeme.parse().unwrap()),
                     lexeme,
@@ -122,7 +129,7 @@ pub fn scan<'a>(source: &'a str) -> Result<Vec<Token<'a>>, Box<dyn Error>> {
                 ));
             }
             some_alpha if some_alpha.is_alphabetic() => {
-                let mut end_idx = cur_idx;
+                let mut end_idx = idx;
                 while let Some((_, maybe_alnum)) = chars.peek() {
                     if !maybe_alnum.is_alphanumeric() {
                         break;
@@ -130,7 +137,7 @@ pub fn scan<'a>(source: &'a str) -> Result<Vec<Token<'a>>, Box<dyn Error>> {
                     end_idx += 1;
                     chars.next();
                 }
-                let lexeme = &source[cur_idx..=end_idx];
+                let lexeme = &src[idx..=end_idx];
                 match string_to_keyword(lexeme) {
                     Some(keyword) => tokens.push(Token::new(keyword, lexeme, line)),
                     None => tokens.push(Token::new(TokenType::Identifier, lexeme, line)),
@@ -141,7 +148,7 @@ pub fn scan<'a>(source: &'a str) -> Result<Vec<Token<'a>>, Box<dyn Error>> {
             '\r' => (),
             '\t' => (),
 
-            not_impl => todo!("haven't implemented for {}", not_impl),
+            _ => error(line, "Unexpected character."),
         }
     }
     tokens.push(Token::new(TokenType::Eof, "", line));
